ca8d62277eabd75cd25366ddf85cc9487dedc116
back-seung <skud113@naver.com>
2022-11-20 19:17:21 +0900

    [Add] LinkedList 수정
    1. peekFirst(), peekLast() 메서드 생성
    2. LinkedList의 인터페이스 ListI 생성
    3. removeFirst(), removeLast() 매개변수 삭제( 불필요한 매개변수 였음 )
    4. LinkedList 테스터 클래스 생성

---
+++ src/main/java/dataStructure/ListI.java
@@ -0,0 +1,1 @@
+package dataStructure;public interface ListI<E> {    public void addFirst(E obj);    public void addLast(E obj);    public E removeFirst();    public E removeLast();    public boolean contains(E obj);    public E remove(E obj);    public E peekFirst();    public E peekLastModTimeComplexity();    public E peekLast();}
